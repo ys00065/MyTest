5ee2596a5e5058db34ebd94661c6c5f4b3913102
Yam <crony_yam@yahoo.com>
2014-10-10 19:38:47 +0100

    first commit

---
+++ src/helloworld/HelloWorldClass.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World STS");
+	}
+
+}
